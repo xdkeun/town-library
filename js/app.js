771da014b7192160b5c334a8917c11d5f9c5c627
Dragonkeun <dragonkeun7@gmail.com>
2022-09-29 20:40:51 +0900

    feat: KakaoAPI jQuery에서 JavaScript 형식으로 변경

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-//창업대전 API
 const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
-    // $.ajax({
-    //     method: "GET",
-    //     url: "https://dapi.kakao.com/v3/search/book?target=title",
-    //     data: { query:"마을" },
-    //     headers: {Authorization: `KakaoAK ${REST_API_KEY}`},
-    // })
-    // .done(function( msg ) {
-    //     console.log(msg);
-    //     console.log(msg.documents[2]);
-    //     $( "div" ).append( `책의 제목 : <strong>${msg.documents[1].title}</strong> <br>`);
-    //     $( "div" ).append( `책 표지 : <img src=${msg.documents[1].thumbnail} /> <br>`);
-    //     $( "div" ).append( `책의 출판사 : ${msg.documents[1].publisher} <br>`);
-    //     $( "div" ).append( `책의 가격 : ${msg.documents[1].price} <br>`);
-    // });
+
+function book() {
+  fetch("https://dapi.kakao.com/v3/search/book?query=우리&size=50", {
+    headers: { Authorization: `KakaoAK ${REST_API_KEY}` },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.documents[0].title);
+      console.log(data.documents[0].url);
+    });
+}
+
+book();
