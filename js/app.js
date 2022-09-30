c5e629e5221f1e08eed78537bce5a5b39f0b7d3e
Dragonkeun <dragonkeun7@gmail.com>
2022-09-30 23:56:48 +0900

    feat: 카테고리 클릭 시 해당 자료 생성되도록 작업

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
 
 function book() {
-  fetch("https://dapi.kakao.com/v3/search/book?query=우리&size=50", {
+  fetch("https://dapi.kakao.com/v3/search/book?query=우리&size=50&", {
+    method : "get",
     headers: { Authorization: `KakaoAK ${REST_API_KEY}` },
   })
     .then((response) => response.json())
@@ -12,3 +13,22 @@ function book() {
 }
 
 book();
+
+const reactCategory = document.querySelector(".react-category");
+const randomBookList = document.querySelector(".random-book-list");
+
+reactCategory.addEventListener("click", reactBook);
+
+function reactBook(){
+  fetch("https://dapi.kakao.com/v3/search/book?query=리액트&size=50&", {
+    method : "get",
+    headers: { Authorization: `KakaoAK ${REST_API_KEY}` },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const newImg = document.createElement("img");
+      randomBookList.appendChild(newImg);
+      console.log(data.documents[0].title);
+      console.log(data.documents[0].url);
+    });
+}
