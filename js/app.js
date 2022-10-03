7449cac68d366995000bcc2021e71dcaf615514b
Dragonkeun <dragonkeun7@gmail.com>
2022-10-04 02:21:40 +0900

    feat: 카테고리 클릭 시 해당하는 책으로 바뀌도록 구현

--- js/app.js
+++ js/app.js
@@ -1,34 +1,28 @@
 const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
 
-function book() {
-  fetch("https://dapi.kakao.com/v3/search/book?query=우리&size=50&", {
+const love = document.querySelector(".love");
+const randomBookImg = document.querySelector(".random-book-img");
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
+
+love.addEventListener("mouseover", loveBook);
+
+function loveBook() {
+  fetch("https://dapi.kakao.com/v3/search/book?query=사랑&size=50&", {
     method: "get",
     headers: { Authorization: `KakaoAK ${REST_API_KEY}` },
   })
     .then((response) => response.json())
     .then((data) => {
-      const firstImg = document.createElement("img");
-      firstImg.src = data.documents[0].thumbnail;
-      randomBookFirst.appendChild(firstImg);
-      firstImg.classList.add("random-book");
-
-      const firstDiv = document.createElement("div");
-      firstDiv.innerHTML = `${data.documents[0].title}
-                            <div>${data.documents[0].price}</div>
-                            <a href=${data.documents[0].url}>More</a>`;
-      randomBookFirst.appendChild(firstDiv);
+      randomBookImg.src = `${data.documents[49].thumbnail}`;
+      bookTitle.innerHTML = `${data.documents[49].title}`;
+      bookAuthor.innerHTML = `${data.documents[49].authors}`;
+      console.log(data);
     });
 }
 
-book();
-
-const reactCategory = document.querySelector(".react-category");
-const randomBookList = document.querySelector(".random-book-list");
-const randomBookFirst = document.querySelector(".random-book-first");
-const randomBookSecond = document.querySelector(".random-book-second");
-const randomBookThird = document.querySelector(".random-book-third");
-
-reactCategory.addEventListener("mouseover", reactBook);
+const react = document.querySelector(".react");
+react.addEventListener("mouseover", reactBook);
 
 function reactBook() {
   fetch("https://dapi.kakao.com/v3/search/book?query=리액트&size=50&", {
@@ -37,11 +31,9 @@ function reactBook() {
   })
     .then((response) => response.json())
     .then((data) => {
-      firstImg.src = data.documents[0].thumbnail;   
-
-      firstDiv.innerHTML = `${data.documents[0].title}
-                            <div>${data.documents[0].price}</div>
-                            <a href=${data.documents[0].url}>More</a>`;
-
+      randomBookImg.src = `${data.documents[1].thumbnail}`;
+      bookTitle.innerHTML = `${data.documents[1].title}`;
+      bookAuthor.innerHTML = `${data.documents[1].authors}`;
+      console.log(data);
     });
 }
