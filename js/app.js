59d6c22c9939c9294cfcb078d3571865dafc2399
Dragonkeun <dragonkeun7@gmail.com>
2022-10-05 22:54:35 +0900

    feat: 30개의 난수를 받아 랜덤한 책 출력

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 const REST_API_KEY = "93965c2d87d826b6a2c82e922835057c";
-
-const love = document.querySelector(".love");
+const randomNumber = Math.floor(Math.random()*30); //난수
 const randomBookImg = document.querySelector(".random-book-img");
 const bookTitle = document.querySelector(".book-title");
 const bookAuthor = document.querySelector(".book-author");
 
+const love = document.querySelector(".love");
 love.addEventListener("mouseover", loveBook);
-
 function loveBook() {
   fetch("https://dapi.kakao.com/v3/search/book?query=사랑&size=50&", {
     method: "get",
@@ -14,16 +13,15 @@ function loveBook() {
   })
     .then((response) => response.json())
     .then((data) => {
-      randomBookImg.src = `${data.documents[49].thumbnail}`;
-      bookTitle.innerHTML = `${data.documents[49].title}`;
-      bookAuthor.innerHTML = `${data.documents[49].authors}`;
-      console.log(data);
+      console.log(data);      
+      randomBookImg.src = `${data.documents[randomNumber].thumbnail}`;
+      bookTitle.innerHTML = `${data.documents[randomNumber].title}`;
+      bookAuthor.innerHTML = `${data.documents[randomNumber].authors}`;
     });
 }
 
 const react = document.querySelector(".react");
 react.addEventListener("mouseover", reactBook);
-
 function reactBook() {
   fetch("https://dapi.kakao.com/v3/search/book?query=리액트&size=50&", {
     method: "get",
@@ -31,9 +29,8 @@ function reactBook() {
   })
     .then((response) => response.json())
     .then((data) => {
-      randomBookImg.src = `${data.documents[1].thumbnail}`;
-      bookTitle.innerHTML = `${data.documents[1].title}`;
-      bookAuthor.innerHTML = `${data.documents[1].authors}`;
-      console.log(data);
+      randomBookImg.src = `${data.documents[randomNumber].thumbnail}`;
+      bookTitle.innerHTML = `${data.documents[randomNumber].title}`;
+      bookAuthor.innerHTML = `${data.documents[randomNumber].authors}`;
     });
 }
