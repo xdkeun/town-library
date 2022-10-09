1e0f7b96c0ac6247c5351873f00819f601e948ab
Dragonkeun <dragonkeun7@gmail.com>
2022-10-09 23:12:40 +0900

    feat: 책 출력, 모달, API연동 완료

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+const modal = document.querySelectorAll(".modal");
+const modalOverlay = document.querySelectorAll(".modal-overlay");
+const modalOpen = document.querySelectorAll(".modal-open");
+const modalClose = document.querySelectorAll(".modal-close");
+
+for(let i = 0; i < 5; i++){
+    const openModal = () =>
+{
+    modal[i].classList.remove("hidden");
+}
+const closeModal = () => {
+    modal[i].classList.add("hidden");
+}
+modalOverlay[i].addEventListener("click", closeModal);
+modalClose[i].addEventListener("click", closeModal);
+modalOpen[i].addEventListener("click",openModal);
+}
