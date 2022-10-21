dbec89e0cda79dba46b8c1f112cbda6da2b09bbd
Dragonkeun <dragonkeun7@gmail.com>
2022-10-21 18:12:47 +0900

    모달창 요소 배치 완료

--- js/all-book.js
+++ js/all-book.js
@@ -8,6 +8,7 @@ function allBookFunc() {
   })
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       allBook.innerHTML = `
             <div class="all-book-box">
                 <div class="all-book-img">
@@ -20,299 +21,33 @@ function allBookFunc() {
                     <button class="all-book-button" id="open">더 보기</button>
                     <div class="modal hidden">
                         <div class="modal-overlay"></div>
+
                         <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
+                        <div class="modal-content-info">
+                            <div class="modal-left-content">
+                                <img src="${data.documents[randomNumber30].thumbnail}">
+                                <div>${data.documents[randomNumber30].title}</div>
+                            </div>
+                            <div class="modal-right-content">
+                                <div>제목 : ${data.documents[randomNumber30].title}</div>
+                                <div>저자 : ${data.documents[randomNumber30].authors}</div>
+                                <div>출판사 : ${data.documents[randomNumber30].publisher}</div>
+                                <div>가격 : ${data.documents[randomNumber30].price}</div>
+                            </div>
+                        </div>
+                        <div class="modal-content-bottom-info">
+                            <div>${data.documents[randomNumber30].contents}</div>
+                            <div class="modal-content-bottom-button">
+                            <a href="${data.documents[randomNumber30].url}">더 보기</a>
                             <button class="close-button">X</button>
+
+                            </div>
                         </div>
                     </div>
                     </div>
-                </div>  
+                    </div>
+                </div>
                 
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
-                <div class="all-book-box">
-                <div class="all-book-img">
-                    <img src="${data.documents[randomNumber30].thumbnail}">
-                </div>
-                <div class="all-book-info">
-                    <h3>${data.documents[randomNumber30].title}</h3>
-                    <h5>${data.documents[randomNumber30].authors}</h5>
-
-                    <button class="all-book-button" id="open">더 보기</button>
-                    <div class="modal hidden">
-                        <div class="modal-overlay"></div>
-                        <div class="modal-content">
-                            ${data.documents[randomNumber30].title}
-                            <button class="close-button">X</button>
-                        </div>
-                    </div>
-                    </div>
-                </div>   
-
                 `;
 
       const openButton = document.querySelectorAll("#open");
